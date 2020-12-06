106a7c22c28c30d881cb4ebecf4efaf65f527529
Rafizqi64 <75040796+Rafizqi64@users.noreply.github.com>
2020-12-06 20:48:06 +0100

    Update huiswerkopgave3.py

--- huiswerkopgave3.py
+++ huiswerkopgave3.py
@@ -15,34 +15,47 @@ def get_fasta_from_file(filename):
     return data
 
 def table_print(sequence_list):
-    headerstr = '{0:^14s} | {1:^12s} | {2:^14s}' # Define table format strings
-    tablestr = '{0:^14s} | {1:^14f} | {2:^14f}'
+    print('|' + '-'*44 + '|')
+    headerstr = '|{0:^14s}|{1:^14s}|{2:^14s}|' # Define table format strings
+    tablestr = '|{0:^14s}|{1:^14.2f}|{2:^14s}|'
     print(headerstr.format('Block', 'CG Ratio', 'PolyA',))
     print(('|'+'-'*14)*3+'|')
-    
+    x_aft = 0
     for i in range(0, len(sequence_list)):
-             C = sequence_list[i].count('C')
-             G = sequence_list[i].count('G')
-             CG_total = C+G
-             CG_Ratio = (CG_total/len(sequence_list[i]))
-#        for x in range(0, len(sequence_list[i])):
-#            mxA = 0
-#            if len(sequence_list.find(row[4]*x)) > mxA:
-#                mxA = len(sequence_list.find(row[4]*x))
-    print(tablestr.format(' ', CG_Ratio, ' '))
-
-full_list = get_fasta_from_file('example.fasta')
-sequencename = full_list[0: 105]
-list_base_blocks = full_list[105 : ]
-print(len(list_base_blocks))
-n = list_base_blocks.split()
-x=100 
-split_list = [list_base_blocks[y-x:y] for y in range(x, len(list_base_blocks)+x, x)]
-print(split_list)
-
-print(table_print('n')) 
         
-# import urllib
+        CG_total = 0
+        mxA = 0
+        A = 0
+        for x in sequence_list[i]:
+            if x in 'CG':
+                CG_total += 1
+            if x in 'A' and A >= 0:
+                A += 1
+                if A > mxA:
+                    mxA = A
+            else:
+                A = 0
+        CG_Ratio = (CG_total/len(sequence_list[i])) 
+ 
+        x_pre = x_aft + 1
+        x_aft = x_aft + len(sequence_list[i])
+        
+        print(tablestr.format(str(x_pre) + ' - ' + str(x_aft), CG_Ratio, str(mxA)))
+    print('|' + '-'*44 + '|')
+    
+full_list = get_fasta_from_file('example.fasta')
+sequencename = full_list.split('\n')[0]
+n = ''.join(full_list.split('\n')[1:])
+
+x = 100 
+split_list = [n[y-x:y] for y in range(x, len(n)+x, x)]
+
+print()
+print(sequencename)
+print()
+table_print(split_list) 
+
+
 # url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=110645916&rettype=fasta"
 # rf = urllib.request.urlopen(url)
 # data = rf.read()
